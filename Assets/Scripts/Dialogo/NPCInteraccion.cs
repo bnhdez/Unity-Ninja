aa2403de0e199daa1a94152a645e553e99941948
Boris <borisnandez93@gmail.com>
2024-11-07 22:12:01 -0600

    Dialogo se cierra al alejarse

--- Assets/Scripts/Dialogo/NPCInteraccion.cs
+++ Assets/Scripts/Dialogo/NPCInteraccion.cs
@@ -10,22 +10,32 @@ public class NPCInteraccion : MonoBehaviour
     [SerializeField] private NPCDialogo npcDialogo;
 
     public NPCDialogo Dialogo => npcDialogo;
-    
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            DialogoManager.Instance.NPCDisponible = this;
-            npcButtonInteractuar.SetActive(true);
-        }
-    }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            DialogoManager.Instance.NPCDisponible = null;
-            npcButtonInteractuar.SetActive(false);
-        }
-    }
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			DialogoManager.Instance.NPCDisponible = this;
+
+			// Mostrar botón de interacción solo si el diálogo no está abierto
+			if (!DialogoManager.Instance.IsDialogoAbierto())
+			{
+				npcButtonInteractuar.SetActive(true);
+			}
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			if (DialogoManager.Instance != null && DialogoManager.Instance.NPCDisponible == this)
+			{
+				DialogoManager.Instance.AbrirCerrarPanelDialogo(false); // Cerrar el diálogo si está abierto
+			}
+
+			DialogoManager.Instance.NPCDisponible = null;
+			npcButtonInteractuar.SetActive(false);
+		}
+	}
 }
